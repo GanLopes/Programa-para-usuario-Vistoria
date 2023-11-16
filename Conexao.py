b99d88f7a80ac1acf0d68373932a4362d6ff24b9
Amorgan M. Lopes <127498760+GanLopes@users.noreply.github.com>
2023-11-16 17:05:09 -0300

    Update Conexao.py

--- Conexao.py
+++ Conexao.py
@@ -2,7 +2,7 @@ import oracledb
 
 def obter_connection():
     try:
-        connection = oracledb.connect(user="RM99585", password="210305", dsn="oracle.fiap.com.br/orcl")
+        connection = oracledb.connect(user="******", password="******", dsn="oracle.fiap.com.br/orcl")
         return connection
     except Exception as e:
         print(f"Erro ao obter conexão: {e}")
